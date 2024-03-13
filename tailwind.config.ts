50454368588b6727a818d38e493efdd6ffd84cad
Jorge Silva <jsilva@bombano.com>
2024-03-12 21:47:34 -0400

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: {
+          900: "#0A0B0F",
+          800: "#141519",
+          700: "#1A1B1F",
+          600: "#2E2F34",
+          500: "#404145",
+          400: "#525357",
+          300: "#646569",
+          200: "#76777C",
+          100: "#88898D",
+          50: "#AAAAAF",
+        },
       },
     },
   },
