9f668824a8d3035f7f01da44722b36a1ec115707
Jorge Silva <jsilva@bombano.com>
2024-03-21 20:54:50 -0400

    Adding ZOD to services

--- app/api/snippet/service.ts
+++ app/api/snippet/service.ts
@@ -1,7 +1,24 @@
 import { db } from "@/lib/db";
-import { Snippet } from "@prisma/client";
+import { Language, Snippet, Technology } from "@prisma/client";
+import { z } from 'zod';
+
+const readAllSnippetSchema = z.object({
+  title : z.string().optional(),
+  content: z.string().optional(),
+  language: z.nativeEnum(Language).optional(),
+  technology: z.nativeEnum(Technology).optional(),
+}).refine((data)=>Object.values(data).some((value => value !== undefined)));
 
 export async function readAllSnippet(filters: Partial<Snippet>) {
+  try {
+    readAllSnippetSchema.parse(filters);
+  } catch(e){
+    return { 
+      error: true,
+      status: 500,
+      message:'Wrong filter provided'+e,
+    };
+  }
   return await db.snippet.findMany(
     { 
       where: {
@@ -11,7 +28,22 @@ export async function readAllSnippet(filters: Partial<Snippet>) {
   );
 }
 
+const createSnippedSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  language: z.nativeEnum(Language),
+  technology: z.nativeEnum(Technology),
+});
 export async function createSnippet(body: Omit<Snippet, 'id'>){
+  try {
+    readAllSnippetSchema.parse(body);
+  } catch(e){
+    return { 
+      error: true,
+      status: 500,
+      message:'Missing or wrong data '+e,
+    };
+  }
   try{
     return await db.snippet.create({data: body});
   }
